65cb7fbb227c127318ca284c6b081cb0bc55e1b7
alvarocojonudo <alvarocojonudo@gmail.com>
2019-11-05 00:27:59 -0600

    nada

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyAgnKcffbIg2huQdwLZsqQVZJBbFa0BEWo",
-    authDomain: "happyend-b2817.firebaseapp.com",
-    databaseURL: "https://happyend-b2817.firebaseio.com",
-    projectId: "happyend-b2817",
-    storageBucket: "happyend-b2817.appspot.com",
-    messagingSenderId: "925930387998",
-    appId: "1:925930387998:web:eefdf8e35ac18a2ee1cc0f"
-  },
-  googleMapsKey: 'AIzaSyB4HCyB5hFbmkOBEUg92yYIY-RKkCMX_5s'
+  production: true
 };
